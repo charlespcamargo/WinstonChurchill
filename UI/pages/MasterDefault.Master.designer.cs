e763ea414ea934992dee55997b5e80242f9cf62b
Alexandre Yembo <alexandreyembo@gmail.com>
2017-05-26 22:54:24 -0300

    exclusao topo

--- UI/pages/MasterDefault.Master.designer.cs
+++ UI/pages/MasterDefault.Master.designer.cs
@@ -49,15 +49,6 @@ namespace UI.pages {
         protected global::System.Web.UI.WebControls.HiddenField hfURLAPI;
         
         /// <summary>
-        /// Topo control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::UI.controls.ucTopo Topo;
-        
-        /// <summary>
         /// ucMenu control.
         /// </summary>
         /// <remarks>
